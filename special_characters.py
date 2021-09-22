e32eab1c628e476a9badea32b3c6420d1f884898
SubRosa02 <90906383+SubRosa02@users.noreply.github.com>
2021-09-22 20:07:53 +0200

    Add files via upload

---
+++ special_characters.py
@@ -0,0 +1,23 @@
+# backspace (\b)
+a = "Back \bSpace!"
+print(a)
+
+# sortores (\n)
+b = "Sor\nTores!"
+print(b)
+
+# tabulator (\t)
+c = "ta\tbu\tla\ttor"
+print(c)
+
+# visszaperjel (\\)
+d = "This will insert one \\ (backslash)."
+print(d)
+
+# aposztrof (\') / (')
+e = 'It\'s alright.'
+print(e)
+
+# idezojel (\")
+f = '\"idezet\"'
+print(f)
