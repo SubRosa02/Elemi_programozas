969a40c4a3ae94091e556de74fcdc2cd3927a659
SubRosa02 <90906383+SubRosa02@users.noreply.github.com>
2021-09-23 16:51:17 +0200

    Add files via upload

---
+++ szavak_hossza.py
@@ -0,0 +1,41 @@
+a = ["almaszár", "kerékgyártó", "Flóra-pihenő", "Malomvölgy", "Misina", "Égervölgyi tó", "Tenkes", "Zsolnay-kút"]
+print(a[0]+": " + str(len(a[0])))
+if len(a[0]) > 10:
+    print(a[0] + " hosszabb 10 karakternel")
+else:
+    print(a[0] + " nem hosszabb 10 karakternel")
+print(a[1]+": " + str(len(a[1])))
+if len(a[1]) > 10:
+    print(a[1] + " hosszabb 10 karakternel")
+else:
+    print(a[1] + " nem hosszabb 10 karakternel")
+print(a[2]+": " + str(len(a[2])))
+if len(a[2]) > 10:
+    print(a[2] + " hosszabb 10 karakternel")
+else:
+    print(a[2] + " nem hosszabb 10 karakternel")
+print(a[3]+": " + str(len(a[3])))
+if len(a[3]) > 10:
+    print(a[3] + " hosszabb 10 karakternel")
+else:
+    print(a[3] + " nem hosszabb 10 karakternel")
+print(a[4]+": " + str(len(a[4])))
+if len(a[4]) > 10:
+    print(a[4] + " hosszabb 10 karakternel")
+else:
+    print(a[4] + " nem hosszabb 10 karakternel")
+print(a[5]+": " + str(len(a[5])))
+if len(a[5]) > 10:
+    print(a[5] + " hosszabb 10 karakternel")
+else:
+    print(a[5] + " nem hosszabb 10 karakternel")
+print(a[6]+": " + str(len(a[6])))
+if len(a[6]) > 10:
+    print(a[6] + " hosszabb 10 karakternel")
+else:
+    print(a[6] + " nem hosszabb 10 karakternel")
+print(a[7]+": " + str(len(a[7])))
+if len(a[7]) > 10:
+    print(a[7] + " hosszabb 10 karakternel")
+else:
+    print(a[7] + " nem hosszabb 10 karakternel")
